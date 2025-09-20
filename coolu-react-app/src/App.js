a4b26dfb554c4b201ea45561c31932342517085a
kokchun <kokchungiang@gmail.com>
2025-09-20 13:16:22 +0200

    changed app

--- coolu-react-app/src/App.js
+++ coolu-react-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>So cool ap</h1>
+        <h1>COOL APP</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
